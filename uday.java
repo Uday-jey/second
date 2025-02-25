bfe519265d94d63221ac166cfea6a0b557c81daa
Uday-jey <karumanchiudayuday@gmail.com>
2025-02-25 22:54:37 +0530

    message

---
+++ uday.java
@@ -0,0 +1,96 @@
+class Demo
+{
+	public static void main(String args[])
+	{
+		int book=1;
+		int days=43;
+		int bookprice=500;
+		int bookrentperday=10;
+		if(days<=10)
+		 System.out.println("nocharges");
+		else if(days<30)
+		 System.out.println(days*bookrentperday);
+		else if(days>=30)
+		 System.out.println(bookprice);
+		
+		/*int a=40;
+		int b=50;
+		int c=89;
+		int maxmarks=300;
+		double total=a+b+c;
+		System.out.println(total);
+		System.out.println((double)(total/maxmarks*100));
+		if(total/maxmarks*100<35)
+		{
+			System.out.println("fail");
+		}
+		else if(total/maxmarks*100<50)
+		{
+			System.out.println("D");
+		}
+		else if(total/maxmarks*100<70)
+		{
+			System.out.println("B");
+		}
+		
+		else if(total/maxmarks*100<80)
+		{
+			System.out.println("B+");
+		}
+		else if(total/maxmarks*100<90)
+		{
+			System.out.println("A");
+		}
+		else if(total/maxmarks*100<=100)
+		{
+			System.out.println("A+");
+		}
+		else 
+		{
+			System.out.println("error");
+		}
+		*/
+
+
+
+
+
+			
+		
+		/*int a=3;
+		if(a==1)
+		{
+			System.out.println("monday");
+		}
+		else if(a==2)
+		{
+			System.out.println("tuesday");
+		}
+		else if(a==3)
+		{
+			System.out.println("wednesday");
+		}
+		else if(a==4)
+		{
+			System.out.println("thursday");
+		}
+		else if(a==5)
+		{
+			System.out.println("friday");
+		}
+		else if(a==6)
+		{
+			System.out.println("saterday");
+		}
+		else if(a==7)
+		{
+			System.out.println("sunday");
+		}
+		else
+		{
+			System.out.println("error");
+		}
+		*/
+
+}
+}
